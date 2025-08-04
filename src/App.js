d27969520cfce661905cf3ef9e61a43e27d58a8f
Rwelch <rwelch.k12@gmail.com>
2025-08-04 11:04:19 -0400

    Update App.js with manual edits

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to K12 TeachSuite Landing Page</h1>
+            <p>This is the main landing page for the project.</p>
+        </div>
+    );
+}
+
+export default App;
