f50c52da0c9c967959ae83efd817aa2b708345a5
Rwelch <rwelch.k12@gmail.com>
2025-08-05 12:20:46 -0400

    update: logo and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-900': '#141414',
+        'blue-800': '#0035F0',
+        'blue-700': '#0035F0',
+        'blue-600': '#0035F0',
+        'blue-100': '#E6ECFA',
+        'yellow-400': '#FFD600',
+        'yellow-100': '#FFF9E3',
+      },
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
