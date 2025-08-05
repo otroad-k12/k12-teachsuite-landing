fce3db164572ccc9291caa567749f3104f2ae8df
Rwelch <rwelch.k12@gmail.com>
2025-08-05 13:54:04 -0400

    Add homepage hero video, icons to pricing, and fix K12 logo contrast for footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        blue: {
-          100: '#E8F6FC',
-          300: '#A2DEFF',
-          400: '#62C7FF',
-          500: '#0035F0',
-          700: '#01308E',
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./public/index.html"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                blue: {
+                    100: '#E8F6FC',
+                    300: '#A2DEFF',
+                    400: '#62C7FF',
+                    500: '#0035F0',
+                    700: '#01308E',
+                },
+                yellow: {
+                    400: '#FFEA2F',
+                },
+                green: {
+                    300: '#8EE68C',
+                },
+                black: {
+                    600: '#141414',
+                },
+            },
+            fontFamily: {
+                sans: ["Work Sans", "sans-serif"],
+                heading: ["Beatrice", "Work Sans", "sans-serif"],
+            },
         },
-        yellow: {
-          400: '#FFEA2F',
-        },
-        green: {
-          300: '#8EE68C',
-        },
-        black: {
-          600: '#141414',
-        },
-      },
-      fontFamily: {
-        sans: ["Work Sans", "sans-serif"],
-        heading: ["Beatrice", "Work Sans", "sans-serif"],
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
